afc8a05cb2bda5350176724a538d87dc54771f98
Jauregui Crespo <jcrespo@medianet.com.ve>
2016-07-12 11:23:57 -0400

    cambios en registro con facebook

--- ApiAngel/src/Controller/ApiController.php
+++ ApiAngel/src/Controller/ApiController.php
@@ -20,15 +20,17 @@ class ApiController extends AppController {
 	}
 
 	//Metodo para la consulta del perfil del usuario
-	public function profile($userId=null){
+	public function profile($token=null){
+
 		if ($this->request->is(['post'])) {
 
-			//cargar modelo 
+			// echo 'Entro al post';
+			//cargar modelo
 			$this->loadModel('Users');
 			//Consulta de usuario
 			$this->set('user',$this->Users
 								->find('all')
-								->where(['Id' => $userId]));
+								->where(['Token'=>$token]));
 		}
 
 		$this->set('_serialize', ['user']);
@@ -38,7 +40,7 @@ class ApiController extends AppController {
 	public function userscore(){
 
 		if ($this->request->is(['post'])) {
-			//cargar modelo 
+			//cargar modelo
 			$this->loadModel('User_Scores');
 			//Consulta de usuario
 			$userscore = $this->User_Scores
@@ -48,14 +50,14 @@ class ApiController extends AppController {
 								->first();
 			echo $userscore;
 		}
-		
+
 		//$this->set('_serialize', ['userscore']);
 	}
 
-	//Funcion para registrar Score 
+	//Funcion para registrar Score
 	public function score($scores = null, $gameId = null, $levelId = null, $userId = null){
 		if ($this->request->is(['post'])) {
-			//Registar Puntuacion 
+			//Registar Puntuacion
 			$scoreTable = TableRegistry::get('UserScores');
 			$score = $scoreTable->newEntity();
 			$score->Score  = $scores;
@@ -63,9 +65,10 @@ class ApiController extends AppController {
 			$score->LevelId = $levelId;
 			$score->UserId = $userId;
 			$score->Created = date("Y-m-d H:i:s");
+;
 
 			//Guardamos los datos
-            if ($scoreTable->save($score)) { 
+            if ($scoreTable->save($score)) {
                 $status = 'The info has been saved.';
             } else {
                 $status = 'The event could not be saved. Please, try again.';
@@ -82,31 +85,83 @@ class ApiController extends AppController {
 	//Funcion para registrar Usuarios
 	public function register($name = null, $lastname = null, $token = null, $email = null, $username = null, $image = null, $methodId = null){
 		if ($this->request->is(['POST'])) {
-			//Registar Puntuacion 
 			$userTable = TableRegistry::get('Users');
-			$user = $userTable->newEntity();
-			$user->Name  = $name;
-			$user->LastName = $lastname;
-			$user->Email = $email;
-			$user->Username = $username;
-			$user->Token = $token;
-			$user->Image = $image;
-			$user->AuthenticationMethodId = 1;//$methodId;
-			$user->StatusId = 1;
-			$user->TermId = 1;
+			
+			//Auth. por Facebook
+			if ($methodId==1) {
+				$exists = $userTable->exists(['Token' => $token]);
+					if (!$exists) {
+						//Registar
+						$user = $userTable->newEntity();
+						$user->Name  = $name;
+						$user->LastName = $lastname;
+						$user->Email = $email;
+						$user->Username = $username;
+						$user->Token = $token;
+						$user->Image = $image;
+						$user->AuthenticationMethodId = 1;//$methodId;
+						$user->StatusId = 1;
+						$user->TermId = 1;
 
-			//Guardamos los datos
-            if ($userTable->save($user)) {
-                $status = 'The user has been saved.';
-            } else {
-                $status = 'The event could not be saved. Please, try again.';
-            }
+						//Guardamos los datos
+						if ($userTable->save($user)) {
+			                $status = 'The user has been saved.';
+			            } else {
+			            	$status = 'Error in register.';
+			            }
+					}else {
+						$this->loadModel('Users');
+						$this->set('user',$this->Users
+										->find('all')
+										/*->select(['Id'=>'Users.Id'
+															,'Token'=>'Users.Token'
+															,'Name'=>'Users.Name'
+															,'LastName'=>'User.LastName'
+															,'Email'=>'Users.Email'])*/
+										->where(['Users.StatusId' => 1, 'Users.Token' => $token])
+										->first());
+						$status = 'The user has been updated.';
+					}
+			}elseif ($methodId==2) {
+				$exists = $userTable->exists(['Token' => $token]);
+					if (!$exists) {
+						//Registar
+						$user = $userTable->newEntity();
+						$user->Name  = $name;
+						$user->LastName = $lastname;
+						$user->Email = $email;
+						$user->Username = $username;
+						$user->Token = $token;
+						$user->Image = $image;
+						$user->AuthenticationMethodId = 2;//$methodId;
+						$user->StatusId = 1;
+						$user->TermId = 1;
+						
+						//Guardamos los datos
+						if ($userTable->save($user)) {
+								$status = 'The user has been saved.';
+						} else {
+							$status = 'Error in register.';
+						} 
+					}else {
+						$this->loadModel('Users');
+						$this->set('user',$this->Users
+										->find('all')
+										->select(['Id'=>'Users.Id'
+															,'Token'=>'Users.Token'
+															,'Name'=>'Users.Name'
+															,'LastName'=>'User.LastName'
+															,'Email'=>'Users.Email'])
+										->where(['Users.StatusId' => 1, 'Users.Token' => $token])
+										->first());
+						$status = 'The user has been updated.';
+					}
+			} 
 
-            $response = array("Status" => $status);
+			//$response = ["Status" => $status, "User" => $user];
 
-            $this->set(compact('response'));
-            $this->set('_serialize', ["response"]);
-            // return $response;
+            //$this->set(compact('user'));
+            $this->set('_serialize', ["user"]);
         }
 
 	}
